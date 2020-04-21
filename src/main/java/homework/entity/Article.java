6c70f46327a764abc1b11934eb0b508a1ffa0999
danelekoo7 <danieljedrus@gmail.com>
2020-04-21 21:15:50 +0200

    * started homework hibernate_spring

---
+++ src/main/java/homework/entity/Article.java
@@ -0,0 +1,99 @@
+package homework.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "articles")
+
+public class Article {
+
+    @Id
+    private Long Id;
+    @Column(length = 200)
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "id_author")
+    private Author author;
+
+    @ManyToMany
+    private List<Category> categories = new ArrayList<>();
+
+    private String content;
+
+    private LocalDateTime created;
+    private LocalDateTime updated;
+
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(LocalDateTime updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "Id=" + Id +
+                ", title='" + title + '\'' +
+                ", author=" + author +
+                ", categories=" + categories +
+                ", content='" + content + '\'' +
+                ", created=" + created +
+                ", updated=" + updated +
+                '}';
+    }
+}
